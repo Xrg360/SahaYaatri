b4b803a5f8914233ea1dec4685d3d583521993c3
Marina2813 <marinaroseshaju123@gmail.com>
2023-09-22 17:58:17 +0530

    added config files

---
+++ backend/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore'; // Include this if you're using Firestore
+
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebaseApp.auth();
+export const firestore = firebaseApp.firestore(); // Include this if you're using Firestore

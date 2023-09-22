361f8ba6c4c144a1fe3d2962a088fad62cf13741
aniladaya <aldrinanila19@gmail.com>
2023-09-22 18:25:24 +0530

    Home And DD unstyled

---
+++ frontend/src/pages/DriverDash.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+
+function DriverDashboard() {
+  const [rideRequests, setRideRequests] = useState([]);
+  const [isAvailable, setIsAvailable] = useState(true);
+
+  // Simulate fetching ride requests from an API or database
+  useEffect(() => {
+    // Replace this with actual API call
+    const fetchRideRequests = async () => {
+      try {
+        // Fetch ride requests data
+        const response = await fetch('api/ride-requests');
+        const data = await response.json();
+        setRideRequests(data);
+      } catch (error) {
+        console.error('Error fetching ride requests: ', error);
+      }
+    };
+
+    fetchRideRequests();
+  }, []);
+
+  const handleToggleAvailability = () => {
+    setIsAvailable(!isAvailable);
+  };
+
+  return (
+    <div className="bg-gray-100 min-h-screen p-8">
+      <div className="max-w-xl mx-auto bg-white p-4 rounded-lg shadow-md">
+        <h1 className="text-3xl font-semibold mb-4">Driver Dashboard</h1>
+        <div className="mb-4">
+          <button
+            onClick={handleToggleAvailability}
+            className={`py-2 px-4 rounded-lg ${
+              isAvailable ? 'bg-green-500 text-white' : 'bg-red-500 text-white'
+            }`}
+          >
+            {isAvailable ? 'Go Offline' : 'Go Online'}
+          </button>
+        </div>
+        <h2 className="text-xl font-semibold mb-2">Pending Ride Requests</h2>
+        <ul>
+          {rideRequests.map((request) => (
+            <li key={request.id} className="mb-2">
+              <div className="bg-gray-200 p-2 rounded-lg">
+                <strong>Passenger: {request.passengerName}</strong>
+                <br />
+                From: {request.pickupLocation} - To: {request.destination}
+                <br />
+                Distance: {request.distance} miles
+                <br />
+                Fare: ${request.fare}
+                <br />
+                <button
+                  className="py-1 px-2 bg-blue-500 text-white rounded-lg mt-2"
+                >
+                  Accept
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default DriverDashboard;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'; // If using React Router for navigation
+
+function Home() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to React Uber</h1>
+      <p className="text-lg text-gray-600 mb-8">Your Reliable Ride Service</p>
+      
+      <div className="bg-white p-6 rounded-lg shadow-md w-96 text-center">
+        <h2 className="text-2xl font-semibold mb-4">Book Your Ride</h2>
+        <label className="block text-gray-600 text-left mb-2">Enter Destination:</label>
+        <input
+          type="text"
+          placeholder="Enter destination"
+          className="w-full p-2 border rounded-lg mb-4 focus:outline-none focus:ring focus:border-blue-300"
+        />
+        
+        <label className="block text-gray-600 text-left mb-2">Select Car Type:</label>
+        <select
+          className="w-full p-2 border rounded-lg mb-4 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          <option value="economy">Economy</option>
+          <option value="standard">Standard</option>
+          <option value="premium">Premium</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
